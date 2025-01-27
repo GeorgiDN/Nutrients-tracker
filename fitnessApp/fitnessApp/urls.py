fe0ee3bbef22c8d8d6290e5c0bfafd7b3ad65fc7
GeorgiDN <go6odn28@gmail.com>
2025-01-27 08:11:59 +0200

    feat(user): add ProfileDeleteView

--- fitnessApp/fitnessApp/urls.py
+++ fitnessApp/fitnessApp/urls.py
@@ -29,6 +29,9 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('profile/', user_views.ProfileView.as_view(), name='profile'),
+    path('profile/<int:pk>/', include([
+        path('delete/', user_views.ProfileDeleteView.as_view(), name='profile-delete'),
+    ]))
 ]
 
 
--- fitnessApp/fitnessApp/users/views.py
+++ fitnessApp/fitnessApp/users/views.py
@@ -1,12 +1,13 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.models import User
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, View
+from django.views.generic import CreateView, View, DeleteView
 from django.contrib import messages
 from django.contrib.auth import login
 
 from fitnessApp.users.forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+from fitnessApp.users.models import UserProfile
 
 
 class AppUserRegisterView(CreateView):
@@ -53,3 +54,25 @@ class ProfileView(LoginRequiredMixin, View):
 
         return render(request, 'users/profile.html', context)
 
+
+class ProfileDeleteView(LoginRequiredMixin, UserPassesTestMixin, View):
+    template_name = 'users/profile-delete-page.html'
+    success_url = reverse_lazy('login')
+
+    def test_func(self):
+        profile = get_object_or_404(UserProfile, pk=self.kwargs["pk"])
+        return self.request.user == profile.user
+
+    def get(self, request, *args, **kwargs):
+        profile = get_object_or_404(UserProfile, pk=self.kwargs["pk"])
+        context = {'profile': profile}
+        return render(request, self.template_name, context)
+
+    def post(self, request, *args, **kwargs):
+        profile = get_object_or_404(UserProfile, pk=self.kwargs["pk"])
+        user = profile.user
+
+        user.delete()
+        profile.delete()
+
+        return redirect(self.success_url)
