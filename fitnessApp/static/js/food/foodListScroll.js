30591e4a49b9bdfee41ff5d67a15339b3e787b8c
GeorgiDN <go6odn28@gmail.com>
2025-01-28 22:22:13 +0200

    refactor(food): add nutrients difference

--- fitnessApp/static/js/food/foodListScroll.js
+++ fitnessApp/static/js/food/foodListScroll.js
@@ -1,19 +1,19 @@
-        document.addEventListener("DOMContentLoaded", function () {
-            document.querySelectorAll('.btn').forEach(button => {
-                button.addEventListener('click', function (event) {
-                    const row = event.target.closest('tr');
-                    if (row && row.id) {
-                        localStorage.setItem('target-food-id', row.id);
-                    }
-                });
-            });
-
-            const targetFoodId = localStorage.getItem('target-food-id');
-            if (targetFoodId) {
-                const targetRow = document.getElementById(targetFoodId);
-                if (targetRow) {
-                    targetRow.scrollIntoView({behavior: 'smooth', block: 'center'});
-                }
-                localStorage.removeItem('target-food-id');
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll('.btn').forEach(button => {
+        button.addEventListener('click', function (event) {
+            const row = event.target.closest('tr');
+            if (row && row.id) {
+                localStorage.setItem('target-food-id', row.id);
             }
         });
+    });
+
+    const targetFoodId = localStorage.getItem('target-food-id');
+    if (targetFoodId) {
+        const targetRow = document.getElementById(targetFoodId);
+        if (targetRow) {
+            targetRow.scrollIntoView({behavior: 'smooth', block: 'center'});
+        }
+        localStorage.removeItem('target-food-id');
+    }
+});
--- fitnessApp/static/js/food/nutritionCompare.js
+++ fitnessApp/static/js/food/nutritionCompare.js
@@ -17,20 +17,34 @@ document.addEventListener("DOMContentLoaded", () => {
     const fatsGoalTdEl = document.querySelector(".fats-goal");
     const fatsGoal = parseFloat(fatsGoalTdEl.textContent);
 
+    const calDiffGramsTdEl = document.querySelector(".calories-diff-grams");
+    const carbsDiffGramsTdEl = document.querySelector(".carbs-diff-grams");
+    const proteinDiffGramsTdEl = document.querySelector(".protein-diff-grams");
+    const fatsDiffGramsTdEl = document.querySelector(".fats-diff-grams");
+
+    calDiffGramsTdEl.textContent = ` ${(totalCalories - caloriesGoal).toFixed(2)}`;
+    carbsDiffGramsTdEl.textContent = ` ${(totalCarbs - carbsGoal).toFixed(2)}`;
+    proteinDiffGramsTdEl.textContent = ` ${(totalProtein - proteinGoal).toFixed(2)}`;
+    fatsDiffGramsTdEl.textContent = ` ${(totalFats - fatsGoal).toFixed(2)}`;
+
     if (totalCalories > caloriesGoal) {
         totalCaloriesTdEl.style.backgroundColor = "yellow";
         totalCaloriesTdEl.style.color = "red";
+        calDiffGramsTdEl.textContent = `+ ${(totalCalories - caloriesGoal).toFixed(2)}`
     }
     if (totalCarbs > carbsGoal) {
         totalCarbsTdEl.style.backgroundColor = "yellow";
         totalCarbsTdEl.style.color = "red";
+        carbsDiffGramsTdEl.textContent = `+ ${(totalCarbs - carbsGoal).toFixed(2)}`;
     }
     if (totalProtein > proteinGoal) {
         totalProteinTdEl.style.backgroundColor = "yellow";
         totalProteinTdEl.style.color = "red";
+        proteinDiffGramsTdEl.textContent = `+ ${(totalProtein - proteinGoal).toFixed(2)}`;
     }
     if (totalFats > fatsGoal) {
         totalFatsTdEl.style.backgroundColor = "yellow";
         totalFatsTdEl.style.color = "red";
+        fatsDiffGramsTdEl.textContent = `+ ${(totalFats - fatsGoal).toFixed(2)}`;
     }
 });
